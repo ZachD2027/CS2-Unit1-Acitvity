8f25a7e9e522b162a091f7026481782ee41f4636
Zach <zachary.dowling2027@gbwl.org>
2025-09-16 14:55:26 +0000

    added Scanner

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+// 1. IMPORT Scanner CLASS
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Let's play MadLibs!");
+
+      // 2. Set up a Scanner OBJECT
+      Scanner input = new Scanner(System.in);
+      // System.in refers to the physical keyboard!
+
+      // 3. Use the Scanner to take input
+      System.out.println("Type your name then press enter: ");
+      String player = input.nextLine();
+      System.out.println("Welcome, " + player);
+
    }
 }
