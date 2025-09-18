f22c895932457cae9e8f8f1b2a498cd57c6744d3
Zach <zachary.dowling2027@gbwl.org>
2025-09-18 18:24:03 +0000

    DONE

--- Main.java
+++ Main.java
@@ -15,5 +15,18 @@ public class Main {
       String player = input.nextLine();
       System.out.println("Welcome, " + player);
 
+      // Create variables for each placeholder word
+      // It was a HOT day in the summer. A boy WALKED to the beach for a NICE day out. While on the BOARDWALK he got bit by a CRAB. He screamed AHHH so loud everyone nearby heard him.
+      String adj1 = "hot";
+      String verb1 = "walked";
+      String adj2 = "nice";
+      String noun = "boardwalk";
+      String noun = "crab";
+      String 
+      // Print your story line by line
+      System.out.println("It was a " + adj1 + "day in summer"); 
+      System.out.println("A boy " + verb1 + "to the beach for a " + adj2 + "nice day out."); 
+   
+
    }
 }
